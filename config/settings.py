a752d1f7905cf5e54c913fd9f5875472202f4493
Paras Singhal <parassinghal@Parass-MacBook-Air.local>
2026-03-03 15:40:01 +0530

    chore: initial commit of backend code

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,15 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 
 # Application definition
 
